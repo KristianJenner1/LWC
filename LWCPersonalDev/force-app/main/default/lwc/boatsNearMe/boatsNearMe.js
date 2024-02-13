6415c354d65faa267283ccfa3a99d6c7d3036767
Kristian Jenner <kristianjenner@brightgen.com>
2024-02-13 11:08:02 +0000

    Update to the map marker icon (cosmetic)

--- LWCPersonalDev/force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
+++ LWCPersonalDev/force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -11,7 +11,7 @@ import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
 
 // constants
 const LABEL_YOU_ARE_HERE = 'You are here!';
-const ICON_STANDARD_USER = 'standard:user';
+const ICON_STANDARD_USER = 'utility:user';
 const ERROR_TITLE = 'Error loading Boats Near Me';
 const ERROR_VARIANT = 'error';
 
@@ -97,6 +97,15 @@ export default class BoatsNearMe extends LightningElement {
                 location: {
                     Latitude: boat.Geolocation__Latitude__s,
                     Longitude: boat.Geolocation__Longitude__s
+                },
+                description: 'This is a great boat!',
+                mapIcon: {
+                    path: 'M48.4,2.5C31.8,3.3,18.2,17.2,17.7,33.9c-0.2,8,2.5,15.3,7.1,21.1c6.8,8.5,12,18.1,15.8,28.3c1.3,3.5,2.5,6.9,3.6,10.1     c1.9,5.5,9.7,5.5,11.6,0c1.1-3.2,2.3-6.6,3.6-10.1c3.7-10.1,8.7-19.7,15.5-28c4.6-5.6,7.4-12.7,7.4-20.5     C82.3,16.4,67,1.7,48.4,2.5z M50,57.1c-12.3,0-22.3-10-22.3-22.3s10-22.3,22.3-22.3s22.3,10,22.3,22.3S62.3,57.1,50,57.1z M34.4,37.9c-0.6,0-1,0.7-0.7,1.2l2.7,4.4c0.5,0.8,1.4,1.3,2.3,1.3h22.4c1,0,1.8-0.5,2.3-1.3l2.7-4.4     c0.3-0.5,0-1.2-0.7-1.2H34.4z M37.8,35.7h8.6c0.4,0,0.6-0.3,0.6-0.6V19.8c0-0.5-0.7-0.7-0.9-0.3l-8.8,15.1C37,35.1,37.3,35.7,37.8,35.7z M50,35.7h12.1c0.5,0,0.8-0.6,0.5-1L50.2,19.1c-0.3-0.4-0.9-0.2-0.9,0.3V35C49.3,35.4,49.6,35.7,50,35.7z',
+                    fillColor: 'black',
+                    fillOpacity: .8,
+                    strokeWeight: 0,
+                    scale: .50,
+                    anchor: {x: 50, y: 75}
                 }
             };
         });
